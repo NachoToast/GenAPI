f2a6e1cab65b23562be28dcaf42872c25aa70859
NachoToast <32235595+NachoToast@users.noreply.github.com>
2026-01-29 16:32:55 +1300

    lint

--- src/helpers/jsDoc/getJsDocExample.ts
+++ src/helpers/jsDoc/getJsDocExample.ts
@@ -3,7 +3,7 @@ import { getJsDocTag } from "./getJsDocTag";
 
 /** Value of the **\@example** JSDoc node. */
 export function getJsDocExample<T>(node: Node, transformFn: (rawValue: string) => T): T | null {
-    const rawValue = getJsDocTag(node, "example", x => x.string());
+    const rawValue = getJsDocTag(node, "example", (x) => x.string());
 
     if (rawValue === null) {
         return null;
